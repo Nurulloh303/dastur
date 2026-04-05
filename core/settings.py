8a1c403ce0533df05033823ffbeea11303c193f2
Nurulloh <nurullohmakhmudaliyev@gmail.com>
2026-04-05 17:03:18 +0500

    cbhschew

--- core/settings.py
+++ core/settings.py
@@ -215,37 +215,31 @@ def get_env_list(name, default=""):
 # =========================
 CORS_ALLOW_ALL_ORIGINS = os.getenv("CORS_ALLOW_ALL_ORIGINS", "False").lower() == "true"
 
-CORS_ALLOWED_ORIGINS = get_env_list(
-    "CORS_ALLOWED_ORIGINS",
-    ",".join([
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ailan.netlify.app",
-        "https://dastur-aw8r.onrender.com",
-    ])
-)
 
 CORS_ALLOW_HEADERS = list(default_headers)
 
-CORS_ALLOW_CREDENTIALS = True
 
 
 # =========================
-# CSRF
-# =========================
-CSRF_TRUSTED_ORIGINS = get_env_list(
-    "CSRF_TRUSTED_ORIGINS",
-    ",".join([
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://ailan.netlify.app",
-        "https://dastur-aw8r.onrender.com",
-    ])
-)
+CORS_ALLOW_ALL_ORIGINS = False
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://ailan.netlify.app",
+    "https://dastur-aw8r.onrender.com",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://ailan.netlify.app",
+    "https://dastur-aw8r.onrender.com",
+]
 
 
 # =========================
